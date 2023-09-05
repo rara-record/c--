f54b11e140e378c4477c3e6f18080ca797d8263b
BORA <a52447879@gmail.com>
2023-09-06 00:35:34 +0900

    first commit

---
+++ test.cpp
@@ -0,0 +1,17 @@
+/**
+ * @brief 학습목표 
+ * C++의 문법에 맞게 식별자를 만들 수 있다.
+ * 기본 자료형의 값을 표현하고, 변수를 만들어 저장하는 표현을 활용할 수 있다.
+ * C++ 언어의 연산자를 사용하여 수식을 작성할 수 있다.
+ * 자료형 사이의 형변환을 할 수 있다.
+ * 
+*/
+
+#include <iostream>
+using namespace std;
+int main() {
+  int name, age;
+  cout << "나의 나이는 : " << endl;
+  cin >> age;
+  cout << age << "입니다" << endl;
+}
